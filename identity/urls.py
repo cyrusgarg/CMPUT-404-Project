fe1474ac434deb12f800280ea1567f97fbde14af
Ashlyn Benoy <ashlynbenoy2004@gmail.com>
2025-02-19 18:26:04 -0700

    added css for files, included urls to view other posts and create/view post for authors

--- identity/urls.py
+++ identity/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views, api_views
 
 app_name = 'identity'
@@ -7,6 +7,7 @@ urlpatterns = [
     # Web views
     path('', views.AuthorListView.as_view(), name='author-list'),
     path('<str:username>/', views.AuthorProfileView.as_view(), name='author-profile'),
-    
+    path('posts/', include('posts.urls', namespace='posts')),
+
     path('webhook/github/', views.github_webhook, name='github-webhook'),
 ]
