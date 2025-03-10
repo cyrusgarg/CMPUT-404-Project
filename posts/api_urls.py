ba6c63b35feae59d89f2c680e923acc749b5c9c2
Ashlyn Benoy <ashlynbenoy2004@gmail.com>
2025-03-10 05:52:49 -0600

    minor

--- posts/api_urls.py
+++ posts/api_urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # API endpoints
     path('<uuid:post_id>/', api_views.get_post_by_fqid, name='api-posts'),
     path('<uuid:post_id>/likes', api_views.local_post_likes, name='local_post_likes'),
-    path('api/posts/<uuid:post_id>/shared/', api_views.shared_post_api, name='shared_post_api'),
+    path('<uuid:post_id>/shared/', api_views.shared_post_api, name='shared_post_api'),
 ]
