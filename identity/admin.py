a2a0ae25cbfc95fa278d855e701dc39b6f60f1e7
cyrusgarg <cyrus1@ualberta.ca>
2025-03-12 03:15:52 +0000

    implementing hosting

--- identity/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Following, FollowRequests, Friendship
-
-admin.site.register(Following)
-admin.site.register(FollowRequests)
-admin.site.register(Friendship)
