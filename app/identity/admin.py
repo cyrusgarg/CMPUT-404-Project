6dd420267bd1f38253872cfb07de9ecd2c84690b
Ashlyn Benoy <ashlynbenoy2004@gmail.com>
2025-03-24 01:10:03 +0000

    fetch remote authors and view remote author profiles done

--- app/identity/admin.py
+++ app/identity/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Following, FollowRequests, Friendship, RemoteNode
+from .models import Following, FollowRequests, Friendship, RemoteNode, RemoteAuthor
 
 admin.site.register(Following)
 admin.site.register(FollowRequests)
@@ -22,3 +22,9 @@ class RemoteNodeAdmin(admin.ModelAdmin):
         queryset.update(is_active=False)
         self.message_user(request, f"{queryset.count()} nodes have been deactivated.")
     deactivate_nodes.short_description = "Deactivate selected nodes"
+
+@admin.register(RemoteAuthor)
+class RemoteAuthorAdmin(admin.ModelAdmin):
+    list_display = ['display_name', 'node', 'host', 'last_updated']
+    list_filter = ['node', 'last_updated']
+    search_fields = ['display_name', 'author_id', 'github']
